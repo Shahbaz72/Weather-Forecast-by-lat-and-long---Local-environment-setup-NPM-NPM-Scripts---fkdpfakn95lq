5f15e6f658b5cd666ae95fb0e8c36f131b8e34e1
Shahbaz72 <alamshahbaz987@gmail.com>
2023-12-13 22:00:26 +0530

    changed

--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,79 @@
-'use client'
 import { useState } from "react";
-export default function WeatherForecastApp() {
-  // State variables
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
 
-  // Fetch weather data
-  const fetchWeatherData = async () => {
+export default function Home() {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [forecastData, setForecastData] = useState([]);
+
+  const handleGetForecast = async (e) => {
+    e.preventDefault();
+
     try {
       const response = await fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longitude}`);
+
+      if (!response.ok) {
+        throw new Error("Unable to fetch forecast data");
+      }
+
       const data = await response.json();
-      setWeatherData(data);
+
+      // Extracting relevant forecast data
+      const extractedData = data.properties.timeseries.slice(0, 30).map((item) => {
+        return {
+          time: new Date(item.time).toLocaleString(),
+          temperature: item.data.instant.details.air_temperature.toFixed(1),
+          summary: item.data.next_1_hours.summary.symbol_code,
+        };
+      });
+
+      setForecastData(extractedData);
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error("Error fetching forecast data:", error);
     }
-  };
-
-  // Handle form submission
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if(isNaN(latitude) || isNaN(longitude)){
-      return;
-     }
-    fetchWeatherData();
-  };
-
-  // Render table rows
-  const renderTableRows = () => {
-    if (!weatherData || !weatherData.properties || !weatherData.properties.timeseries) {
-      return null;
-    }
-
-    const first30TimeSeries = weatherData.properties.timeseries.slice(0, 30);
-
-    return first30TimeSeries.map((forecast) => {
-      const time = new Date(forecast.time).toLocaleString();
-      const temperature = forecast.data.instant.details.air_temperature.toFixed(1);
-      const summary = forecast.data.next_1_hours.summary.symbol_code;
-
-      return (
-        <tr key={forecast.time}>
-          <td>{time}</td>
-          <td>{temperature} °C</td>
-          <td>{summary}</td>
-        </tr>
-      );
-    });
   };
 
   return (
-    <div id="root">
-      <h1>Weather Forecast</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Latitude:
-          <input type="text" className="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
-        </label>
-        <label>
-          Longitude:
-          <input type="text" className="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
-        </label>
-        <button type="submit">Get Forecast</button>
-      </form>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Temperature (°C)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>{renderTableRows()}</tbody>
-      </table>
-    </div>
+    <>
+      <div id="root">
+        <h1>Weather Forecast</h1>
+        <form onSubmit={handleGetForecast}>
+          <label htmlFor="latitude">Latitude:</label>
+          <input
+            type="text"
+            id="latitude"
+            className="latitude"
+            value={latitude}
+            onChange={(e) => setLatitude(e.target.value)}
+          />
+          <label htmlFor="longitude">Longitude:</label>
+          <input
+            type="text"
+            id="longitude"
+            className="longitude"
+            value={longitude}
+            onChange={(e) => setLongitude(e.target.value)}
+          />
+          <button type="submit">Get Forecast</button>
+        </form>
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Temperature (°C)</th>
+                <th>Summary</th>
+              </tr>
+            </thead>
+            <tbody>
+              {forecastData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.time}</td>
+                  <td>{item.temperature}</td>
+                  <td>{item.summary}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+      </div>
+    </>
   );
 }
